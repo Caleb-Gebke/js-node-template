dac76f279cd00accd01fc7874dd3cee332316973
Caleb Gebke <caleb.gebke@student.swic.edu>
2025-02-27 16:02:15 -0600

    All six funtions complete!

--- src/functions.js
+++ src/functions.js
@@ -2,28 +2,50 @@ const order = {
     price: 10,
     quantity: 2,
 }
-console.log(order.price * order.quantity);
+function calculateTotal(order) {
+    return order.price * order.quantity;
+}
+console.log(calculateTotal(order));
+
 
 const name = {
     firstName: 'John',
     lastName: 'Doe',
 }
-console.log(`${name.lastName} ${name.firstName}`);
+function formatUsername(name) {
+    return `${name.lastName}, ${name.firstName}`;
+}
+console.log(formatUsername(name));
+
 
 const WAZZUP = 'Hello' + ' ' + 'World!';
-console.log(WAZZUP);
+function greet() {
+    return WAZZUP;
+}
+console.log(greet());
+
 
 const cart = {
     price: 100,
     tax: 0.07,
 }
-console.log((cart.price * cart.tax) + cart.price);
+
+function Total(cart) {
+    return cart.price + cart.price * cart.tax;
+}
+console.log(Total(cart));
+
+
 
 const nameInfo = {
     firstName: 'John',
     lastName: 'Doe',
 }
-console.log(`${nameInfo.firstName} ${nameInfo.lastName}`);
+function displayFullName(nameInfo) {
+    return `${nameInfo.firstName} ${nameInfo.lastName}`;
+}
+console.log(displayFullName(nameInfo));
+
 
 const character = {
     name: 'Booker Dewitt',
