94e2804e61bfbc8af80853e0d7046c6f17ab5fda
Caleb Gebke <caleb.gebke@student.swic.edu>
2025-02-27 11:47:56 -0600

    add string formatting code

---
+++ src/strings.js
@@ -0,0 +1,8 @@
+const Name = 'Caleb Gebke';
+const CourseName = 'CIS 177';
+const Grade = '30/30';
+const Assignment = `${Name} in class ${CourseName} will receive the grade ${Grade}`;
+const Assignment2 = Name + ' in class ' + CourseName + ' will receive the grade ' + Grade;
+console.log(Assignment);
+console.log(Assignment2);
+// Output:  Caleb Gebke in class CIS 177 will receive the grade 30/30
